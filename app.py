cd0b8767672184882156ff458a5a6615aac4a6e5
akansha131313 <akanshaanand06@gmail.com>
2025-06-15 18:30:44 -0400

    fixed legend

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ ticker = st.text_input("Enter a stock ticker:", "AAPL")
 
 if ticker:
     df = stockAnalyze(ticker)
-    st.line_chart(df[["Close", "Moving Avg over 20 days", "Moving average over 50 days"]])
+    st.line_chart(df[["Close", "MA20", "MA50"]])
