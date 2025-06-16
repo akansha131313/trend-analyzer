eb6cd97e2489176796615a20905c138ebdc71ed7
akansha131313 <akanshaanand06@gmail.com>
2025-06-15 22:45:46 -0400

    g

--- stockData.py
+++ stockData.py
@@ -1,16 +1,15 @@
 import yfinance as yf
 import pandas as pd
-import matplotlib.pyplot as ml
 
 #stock analyzer method
-def stockAnalyze(stocks):
+def stockAnalyze(name):
     #gets the rolling avg for the close time
-    ticker = yf.Ticker(stocks)
+    ticker = yf.Ticker(name)
     df = ticker.history(period="6mo")
     df['MA20'] = df['Close'].rolling(window=20).mean()
     df['MA50'] = df['Close'].rolling(window=50).mean()
     return df
 
 
-
+print(stockAnalyze("MSFT"))
 
