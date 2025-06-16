96c2d39da48b1f81a78a6059333f08a3fb9a7558
akansha131313 <akanshaanand06@gmail.com>
2025-06-15 22:08:44 -0400

    m

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 
 st.title("Stocks Trend Analyzer--Happy Father's Day!!")
 
-ticker = input("Enter a stock ticker:")
-data = st.text_input("Enter a stock ticker:", ticker )
+data = st.text_input("Enter a stock ticker:")
 
 if data:
     st.line_chart(stockAnalyze(data))
