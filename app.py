dadddd9347ff5213ce12bf8853f1d1c97b64aaee
akansha131313 <akanshaanand06@gmail.com>
2025-06-15 22:19:21 -0400

    fix

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ st.title("Stocks Trend Analyzer--Happy Father's Day!!")
 
 ticker = input("Enter a stock ticker:")
 data = st.text_input("Enter a stock ticker:")
-df = stockAnalyze(ticker)
+
 if data:
-    st.line_chart(st.dataframe(df))
+    df = stockAnalyze(ticker)
+    st.line_chart(df[["Close", "MA20", "MA50"]])
