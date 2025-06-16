088f240a1cfc6495da3ac02a6cc41e5e6dc00d1f
akansha131313 <akanshaanand06@gmail.com>
2025-06-15 22:30:46 -0400

    n

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 
 st.title("Stocks Trend Analyzer--Happy Father's Day!!")
 
-ticker = st.text_input("Enter a stock ticker:", "AAPL")
+ticker = st.text_input("Enter a stock ticker:", "AAPL").strip().upper()
 
 if ticker:
     df = stockAnalyze(ticker)
     st.success(f"Data loaded for {ticker}")
     st.dataframe(df.tail()) 
     st.line_chart(df[["Close", "MA20", "MA50"]])
+
