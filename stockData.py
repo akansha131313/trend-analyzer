73ff6eddafc4aeed72c451b34e0fc0b3e78c43eb
akansha131313 <akanshaanand06@gmail.com>
2025-06-15 21:49:39 -0400

    t

--- stockData.py
+++ stockData.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as ml
 #stock analyzer method
 def stockAnalyze(stocks):
     #gets the rolling avg for the close time
-    ticker = yf.Ticker(stocks)
-    df = ticker.history(period="6mo")
-    df['MA20'] = df['Close'].rolling(window=20).mean()
-    df['MA50'] = df['Close'].rolling(window=50).mean()
-    df[['Close', 'MA20', 'MA50']].plot(figsize=(10,6), title="Moving Average Trend of ")
-    ml.grid(True)
-    ml.show()
-    return df
+    for stock in stocks:
+        ticker = yf.Ticker(stock)
+        df = ticker.history(period="6mo")
+        df['MA20'] = df['Close'].rolling(window=20).mean()
+        df['MA50'] = df['Close'].rolling(window=50).mean()
+        df[['Close', 'MA20', 'MA50']].plot(figsize=(10,6), title="Moving Average Trend of ")
+        ml.grid(True)
+        ml.show()
+        return df
 
 
 
