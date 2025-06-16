498446130babf930bb5f7920e99677cc3ea82f52
akansha131313 <akanshaanand06@gmail.com>
2025-06-15 22:51:28 -0400

    fix

--- app.py
+++ app.py
@@ -8,10 +8,4 @@ ticker = st.text_input("Enter a stock ticker:", "AAPL")
 
 if ticker:
     df = stockAnalyze(ticker)
-    if df is None or df.empty:
-        st.error("No data found. Please check the ticker symbol or try again later.")
-    else:
-        st.success(f"Data loaded for {ticker}")
-        st.subheader("Historical Data (Last 5 rows)")
-        st.dataframe(df.tail())
-        st.line_chart(df[["Close", "MA20", "MA50"]])
+    st.line_chart(df[["Close", "MA20", "MA50"]])
--- stockData.py
+++ stockData.py
@@ -1,13 +1,16 @@
 import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as ml
 
 #stock analyzer method
-def stockAnalyze(name):
-    #gets the rolling avg for the close time
-    ticker = yf.Ticker(name)
-    df = ticker.history(period="6mo")
-    df['MA20'] = df['Close'].rolling(window=20).mean()
-    df['MA50'] = df['Close'].rolling(window=50).mean()
-    return df
-
-
+def stockAnalyze(stocks):
+    for symbol in stocks:
+        #gets the rolling avg for the close time
+        ticker = yf.Ticker(symbol)
+        df = ticker.history(period="6mo")
+        df['MA20'] = df['Close'].rolling(window=20).mean()
+        df['MA50'] = df['Close'].rolling(window=50).mean()
+        df[['Close', 'MA20', 'MA50']].plot(figsize=(10,6), title="Moving Average Trend of ")
+        ml.grid(True)
+        ml.show()
+        return df
